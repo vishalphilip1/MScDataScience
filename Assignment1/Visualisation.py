ab0bad8976bcb83b007f38572fe695dca94d8649
vishalphilip1 <125706610+vishalphilip1@users.noreply.github.com>
2023-03-03 14:39:07 +0000

    Update Visualisation.py

--- Assignment1/Visualisation.py
+++ Assignment1/Visualisation.py
@@ -4,7 +4,8 @@ Created on Tue Feb 28 11:44:11 2023
 
 @author: Vishal Philip
 
-This script consists of functions for plotting line charts, bar charts and pie charts using matplotlib library. 
+This script consists of functions for plotting line charts,
+bar charts and pie charts using matplotlib library.
 It also reads data from excel files using pandas.
 
 """
@@ -13,60 +14,70 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-''' Created a function for plotting line chart. '''
+
 def line_plot(x_axis, y_axis, name, col):
-    return plt.plot(x_axis,y_axis, '.-',label= name, color= col) 
+    ''' Created a function for plotting line chart '''
+    return plt.plot(x_axis, y_axis, '.-', label=name, color=col)
 
-''' Created a function for plotting bar graph. '''  
-def bar_plot(ypos,Year_data,a):
-    fig = plt.figure(figsize=(10, 5))
-    plt.xticks(ypos,Countries_data)
-    return plt.bar(ypos,Year_data, width = a)
 
-''' Created a function for plotting pie chart. '''
-def plot_pie(Pie_Data,Pie_label):
-    fig = plt.figure(figsize=(10, 5))
-    return plt.pie(Pie_Data, labels = Pie_label,autopct='%1.1f%%' )
+def bar_plot(ypos, Year_data, a):
+    ''' Created a function for plotting bar graph. '''
 
-'''Reading data from excel file '''
-data1 = pd.read_excel(r"C:\Users\acer\OneDrive - University of Hertfordshire\Assignment\Applied data science\Assignment1\Mobile Cellular Subscription per 100.xlsx")
+    plt.figure(figsize=(10, 5))
+    plt.xticks(ypos, Countries_data)
+    return plt.bar(ypos, Year_data, width=a)
 
-#dropping unwanted data
+
+def plot_pie(Pie_Data, Pie_label):
+    ''' Created a function for plotting pie chart. '''
+    plt.figure(figsize=(10, 5))
+    return plt.pie(Pie_Data, labels=Pie_label, autopct='%1.1f%%')
+
+
+# Reading data from excel file 
+data1 = pd.read_excel("Mobile Cellular Subscription per 100.xlsx")
+
+# dropping unwanted data
 data2 = data1.drop(5, axis=0)
 
 ''' Plotting line chart using the function line_plot'''
 plt.figure()
-line_plot(data2.Year, data2.Afghanistan,"Afghanistan","red")
-line_plot(data2.Year, data2.Bangladesh, "Bangladesh","blue")
-line_plot(data2.Year, data2.Bhutan,"Bhutan","green")
-line_plot(data2.Year, data2.India, "India","black")
-line_plot(data2.Year, data2.Nepal, "Nepal","yellow")
-line_plot(data2.Year, data2.Pakistan,"Pakistan","orange")
-line_plot(data2.Year, data2.Bangladesh, "SriLanka","violet")
-#Creating labels
+
+line_plot(data2.Year, data2.Afghanistan, "Afghanistan", "red")
+line_plot(data2.Year, data2.Bangladesh, "Bangladesh", "blue")
+line_plot(data2.Year, data2.Bhutan, "Bhutan", "green")
+line_plot(data2.Year, data2.India, "India", "black")
+line_plot(data2.Year, data2.India, "India", "black")
+line_plot(data2.Year, data2.Nepal, "Nepal", "yellow")
+line_plot(data2.Year, data2.Pakistan, "Pakistan", "orange")
+line_plot(data2.Year, data2.SriLanka, "SriLanka", "violet")
+
+# Creating labels
 plt.xlabel("Years", size=10)
 plt.ylabel("Subscription per 100", size=10)
-#creating Title
+
+# creating Title
 plt.title("Mobile Cellular Subscription per 100 people")
-plt.legend()
-#Saving image
+plt.legend(fontsize=8)
+
+# Saving image
 plt.savefig("lineplot.png")
 plt.show()
 
 
-''' Plotting bar graph using the function bar_plot'''
-#Reading data from excel file
+# Plotting bar graph using the function bar_plot
+# Reading data from excel file
 newdata = pd.read_excel("Mobile Cellular Subscription Copy.xlsx")
 Countries_data = newdata.Countries
 Year_data = newdata.Year_2015
 ypos = np.arange(len(Countries_data))
-bar_plot(ypos,Year_data,0.5)
+bar_plot(ypos, Year_data, 0.5)
 plt.xlabel("Countries", size=10)
 plt.ylabel("Subscription per 100 People", size=10)
 plt.title("Mobile Cellular Subscription per 100 people in 2015")
 plt.savefig("bargraph1.png")
 Year_data = newdata.Year_2005
-bar_plot(ypos,Year_data,0.5)
+bar_plot(ypos, Year_data, 0.5)
 plt.xlabel("Countries", size=10)
 plt.ylabel("Subscription per 100 People", size=10)
 plt.title("Mobile Cellular Subscription per 100 people in 2005")
@@ -74,7 +85,7 @@ plt.savefig("bargraph2.png")
 plt.show()
 
 
-''' Plotting pie chart using the function plot_pie'''
+# Plotting pie chart using the function plot_pie
 pie_data = pd.read_excel("ForestArea.xlsx")
 Countries_name = pie_data.Countries
 Forest_Data2015 = pie_data.Year_2015
@@ -86,4 +97,3 @@ plot_pie(Forest_Data2005, Countries_name)
 plt.title("Forest Area of South Asian Countries (2005)")
 plt.savefig("piechart2.png")
 plt.show()
-
